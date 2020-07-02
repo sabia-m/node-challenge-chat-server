43fcff989ddbda5f47543c8d043476fa04ee113f
Sabia <sabiamushtaq2000@gmail.com>
2020-07-02 20:38:30 +0100

    lvl 200 done

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const bodyP = require("body-parser");
 
 const app = express();
 
+
 app.use(cors());
+app.use(bodyP());
 
 const welcomeMessage = {
   id: 0,
@@ -11,13 +14,37 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+console.log(messages)
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
 });
+
+app.get("/messages", (request, response) => {
+  response.json(messages);
+});
+
+app.post("/messages", (request, response) => {
+  if (request.body.text === '' || request.body.from === ''){
+    return response.status(404)
+  } else {
+  messages.push(request.body);
+  response.json({ success: true })};
+});
+
+
+app.get("/messages/:messageId", (request, response) => {
+  const messageId = request.params.id
+  const chosenMessage = messages.find(message => message.messageId === messageId)
+  response.json(chosenMessage)
+})
+
+app.delete("/messages/:messageId", (request, response) => {
+  const messageId = request.params.id
+  messages = messages.filter(message => message.messageId !== messageId)
+  response.json({success: true})
+})
 
 app.listen(process.env.PORT);
