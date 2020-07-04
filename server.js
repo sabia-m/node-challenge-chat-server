bd4fc1f3c0a80799ee01cc4a8c2a79d1e842d9a8
Sabia <sabiamushtaq2000@gmail.com>
2020-07-04 19:43:48 +0100

    lvl 300 done

--- server.js
+++ server.js
@@ -23,7 +23,67 @@ let messages = [
   id: 2,
   from: "Todd",
   text: "Roadside",
-}
+},
+    {
+  id: 3,
+  from: "Alina",
+  text: "Hey queen",
+},
+  {
+  id: 4,
+  from: "Serena",
+  text: "BTS",
+},
+  {
+  id: 5,
+  from: "J'uenelle",
+  text: "Dessert",
+},
+    {
+  id: 6,
+  from: "Aqil",
+  text: "Ghibli",
+},
+  {
+  id: 7,
+  from: "Jordanna",
+  text: "Fenty",
+},
+  {
+  id: 8,
+  from: "Aliyah",
+  text: "Metts",
+},
+    {
+  id: 9,
+  from: "Sonjide",
+  text: "Cupcakes",
+},
+  {
+  id: 10,
+  from: "Cynthia",
+  text: "JS",
+},
+  {
+  id: 11,
+  from: "Roxana",
+  text: "Node",
+},
+{
+  id: 12,
+  from: "Mohbeen",
+  text: "Netflix",
+},
+  {
+  id: 13,
+  from: "Fas",
+  text: "Pokemon",
+},
+  {
+  id: 14,
+  from: "Yas",
+  text: "Ironing",
+},
 ];
 
 //let messages = [welcomeMessage];
@@ -40,11 +100,20 @@ app.get("/messages", (request, response) => {
 
 app.post("/messages", (request, response) => {
   if (request.body.text === '' || request.body.from === ''){
-  return response.status(404)
+  return response.status(404).send("Please fill all fields!")
   } else {
   messages.push(request.body);
   response.json({ success: true })};
 });
+
+app.get('/messages/search', (request, response) => {
+  const search = request.query.text
+  response.json(messages.filter(message => message.text.toLowerCase().includes(search.toLowerCase())))
+})
+
+app.get('/messages/latest', (request, response) => {
+  response.json(messages.slice(messages.length - 10, messages.length))
+})
 
 app.get("/messages/:id", (request, response) => {
   const messageId = request.params.id
